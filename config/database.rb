4cd22ee07504c32d18a995f07f9107962b00937b
dzikibeduin <olszowy.patryk@gmail.com>
2025-08-03 21:10:48 +0200

    fix(database): fixing connection to db

--- config/database.rb
+++ config/database.rb
@@ -1,3 +1,18 @@
 require 'sequel'
 
-DB = Sequel.connect(ENV['DATABASE_URL'] || 'postgres://ruby_user:ruby_pass@localhost:5432/ruby_app_dev')
+DB = nil
+MAX_ATTEMPTS = 10
+WAIT_TIME = 2 # sekundy
+
+MAX_ATTEMPTS.times do |i|
+  begin
+    DB = Sequel.connect('postgres://postgres:postgres@db:5432/ruby_app_dev')
+    puts "✅ Połączono z bazą danych"
+    break
+  rescue Sequel::DatabaseConnectionError => e
+    puts "⏳ Próba #{i+1}/#{MAX_ATTEMPTS}: baza niedostępna (#{e.message})"
+    sleep WAIT_TIME
+  end
+end
+
+raise "❌ Nie udało się połączyć z bazą po #{MAX_ATTEMPTS} próbach" unless DB
